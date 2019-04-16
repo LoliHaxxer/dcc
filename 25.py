e3ae787fc6f7ec7353d8b3f0ea782c106257e39f
LoliHaxxer <49691454+LoliHaxxer@users.noreply.github.com>
2019-04-16 19:54:10 +0200

    Add files via upload

---
+++ 25.py
@@ -0,0 +1,30 @@
+tests=[["ray","ra.",],["raymond","ra.",],["chat",".*at",],["chats",".*at",],]
+def match(s,re,):
+	pp=[]
+	for ree in re:
+		if ree=='*':pp[len(pp)-1][1]=1
+		else:pp.append([ree,0,],)
+	ls,lre,=len(s),len(pp),
+	def rem(s,re,):
+		if re==".":return True
+		return s==re
+	def ppmatch(s,re,si,rei,):
+		if rei>=lre:
+			if si>=ls:
+				return True
+			return False
+		reat=re[rei]
+		type=reat[1]
+		if type:
+			sp=0
+			while 1:
+				if ppmatch(s,re,si+sp,rei+1,):
+					return True
+				sp+=1
+				if si+sp>=ls or not rem(s[si+sp],reat[0]): return False
+		else:
+			if not rem(s[si],reat[0]): return False
+			return ppmatch(s,re,si+1,rei+1,)
+	return ppmatch(s,pp,0,0)
+for test in tests: print(test,match(*test),sep=" :\n")
+input()
